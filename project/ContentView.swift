ccd74337634039ac29b2a99848b5cb532d9d31df
dmathews0831 <dmathews0831@vt.edu>
2026-02-09 15:14:41 -0500

    initial xcode files

---
+++ project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  project
+//
+//  Created by Dylan Robert Mathews on 2/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by Dylan Robert Mathews on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
