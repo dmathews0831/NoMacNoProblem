19b088850e9f08633e0fb8c317136571bce777b8
dmathews0831 <165334622+dmathews0831@users.noreply.github.com>
2026-02-23 15:31:22 -0500

    attempted to add watch target

---
+++ watchTarget Watch App/watchTargetApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchTargetApp.swift
+//  watchTarget Watch App
+//
+//  Created by Dylan Robert Mathews on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct watchTarget_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
